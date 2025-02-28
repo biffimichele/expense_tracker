61a252b8bc0e5108c2fb131c2ac07c634d977679
Michele Biffi <michelebiffi98@gmail.com>
2025-02-28 01:21:42 +0100

    added some theming customizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:applicazione/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 81, 46, 164),
+);
+
 void main() {
   runApp(
     MaterialApp(
+      theme: ThemeData(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.primary,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kColorScheme,
+      ),
       home: Expenses(),
     ),
   );
